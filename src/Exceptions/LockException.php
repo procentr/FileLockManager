1ea7c6546385cf351b05a5c124677f5317bfbc85
Igor Lepekhin <1248@bk.ru>
2025-05-18 13:16:05 +0300

    Change exception messages

--- src/Exceptions/LockException.php
+++ src/Exceptions/LockException.php
@@ -17,7 +17,7 @@ class LockException extends RuntimeException
      * @param int $code Error code (default 0).
      * @param Throwable|null $previous Previous exception, if any.
      */
-    public function __construct(string $message = "File not found", int $code = 0, Throwable $previous = null)
+    public function __construct(string $message = "Failed to acquire lock", int $code = 0, Throwable $previous = null)
     {
         parent::__construct($message, $code, $previous);
     }
